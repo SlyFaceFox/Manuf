9c2261ef02051e74626c7fa37c80c0c1a45a97b6
Huawei <Huawei@LAPTOP-5LRHA01L>
2023-04-17 12:17:14 +0300

    Добавьте файлы проекта.

---
+++ Manuf/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace Manuf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DGridSale.ItemsSource = ManafactuterEntities.GetContext().sale.ToList();
+        }
+        private void Button_Click(Object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
